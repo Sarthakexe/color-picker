251f6ce1aa60d3ae42c4a3ce4746ee7b2bcb8a4c
Sarthakexe <144121726+Sarthakexe@users.noreply.github.com>
2025-06-04 11:51:26 +0530

    Create ColorPicker.jsx

---
+++ ColorPicker.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+function ColorPicker(){
+
+    const [color, setColor] = useState("FFFFFF")
+
+    function handleColorChange(event){
+        setColor(event.target.value)
+    }
+
+    return(<div className="color-picker-container">
+                <h1>Color Picker</h1>
+                <div className="swatch-card">
+                  <div className="swatch-color" style={{ backgroundColor: color }}></div>
+                         <p className="swatch-label">{color.toUpperCase()}</p>
+                    </div>
+    
+                <label>Select a Color:</label>
+                <input type="color" value={color} onChange={handleColorChange}/>
+            </div>);
+}
+
+export default ColorPicker
